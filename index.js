0e45734b01f50dbb6cc526212e7532072663ffbb
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-02-08 08:57:41 -0800

    Add Book Constructor

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let myLibrary = [];
 
 function Book(author, title, noOfPages, readQ) {
-  this.author;
-  this.title;
-  this.noOfPages;
-  this.readQ;
+  this.author = author;
+  this.title = title;
+  this.noOfPages = noOfPages;
+  this.readQ = readQ;
 }
 
+let 
+
+
 function addBookToLibrary() {}
+
+
+function displayBooks(){
+
+}
