514eac242a407a6abe8841c71ad44fc5cd0116e9
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-02-08 10:00:00 -0800

    Create function displayBooks() that display books on the page

--- index.js
+++ index.js
@@ -8,12 +8,36 @@ function Book(author, title, noOfPages, readQ) {
 }
 
 let deepWork = new Book("Cal Newport", "Deep Work", 255, true);
+let atomicHabits = new Book("James Clear", "Atomic Habits", 300, false);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
 addBookToLibrary(deepWork);
-console.log(myLibrary);
+addBookToLibrary(atomicHabits);
 
-function displayBooks() {}
+let divCardsContainer = document.querySelector("#container");
+function displayBooks(libraryArray) {
+  for (let i = 0; i < libraryArray.length; i++) {
+    let card = document.createElement("div");
+    let title = document.createElement("p");
+    let author = document.createElement("p");
+    let noOfPages = document.createElement("p");
+    let readQ = document.createElement("p");
+
+    title = libraryArray[i].title;
+    author = libraryArray[i].author;
+    noOfPages = libraryArray[i].noOfPages;
+    readQ = libraryArray[i].readQ;
+
+    card.append(title);
+    card.append(author);
+    card.append(noOfPages);
+    card.append(readQ);
+
+    divCardsContainer.append(card);
+  }
+}
+
+displayBooks(myLibrary);
